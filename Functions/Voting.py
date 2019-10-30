4990dbcc37df7b896c31f6e15b3a0788870f77b4
Joel Medeiros <jooel.medeiros@hotmail.com>
2019-10-29 21:18:26 -0300

    Improving if validation of between values and variables

--- Functions/Voting.py
+++ Functions/Voting.py
@@ -2,7 +2,7 @@ def vote(birthYear):
     from datetime import date
     age = date.today().year - birthYear
     ageString = f'With {age} years old'
-    if (18 > age > 16 or age >= 65):
+    if (16 <= age < 18 or age >= 65):
         return (f'{ageString} the vote is optional')
     elif (age < 16):
         return (f'{ageString} can not vote')
